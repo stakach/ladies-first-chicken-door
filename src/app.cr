a7c980cb05ee9167ed7363a76fdfff543035005a
Stephen von Takach <steve@place.technology>
2023-10-22 08:12:27 +1100

    feat: bind to IPv4 and IPv6

--- src/app.cr
+++ src/app.cr
@@ -4,7 +4,6 @@ require "./constants"
 module DoorCtrl
   # Server defaults
   port = DEFAULT_PORT
-  host = DEFAULT_HOST
   process_count = DEFAULT_PROCESS_COUNT
   docs = nil
   docs_file = nil
@@ -13,7 +12,6 @@ module DoorCtrl
   OptionParser.parse(ARGV.dup) do |parser|
     parser.banner = "Usage: #{PROGRAM_NAME} [arguments]"
 
-    parser.on("-b HOST", "--bind=HOST", "Specifies the server host") { |h| host = h }
     parser.on("-p PORT", "--port=PORT", "Specifies the server port") { |p| port = p.to_i }
 
     parser.on("-w COUNT", "--workers=COUNT", "Specifies the number of processes to handle requests") do |w|
@@ -75,7 +73,7 @@ end
 require "./config"
 
 module DoorCtrl
-  server = ActionController::Server.new(port, host)
+  server = ActionController::Server.new(port, "0.0.0.0")
 
   # (process_count < 1) == `System.cpu_count` but this is not always accurate
   # Clustering using processes, there is no forking once crystal threads drop
@@ -105,6 +103,7 @@ module DoorCtrl
 
   # Start the server
   server.run do
+    server.socket.bind_tcp("::", port, ActionController::Server::REUSE_DEFAULT)
     puts "Listening on #{server.print_addresses}"
   end
 
